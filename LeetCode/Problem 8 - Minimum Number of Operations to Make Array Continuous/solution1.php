f5aec531e59c7eabb46ffe432e8e722adc1a1dbc
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2023-10-10 22:17:55 +0300

    8th Question Wrong Answer
    Wrong Answer
    9 / 62 testcases passed

---
+++ LeetCode/Problem 8 - Minimum Number of Operations to Make Array Continuous/solution1.php
@@ -0,0 +1,30 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function minOperations($nums)
+    {
+        sort($nums);
+        print_r($nums);
+        $oldnumber = $nums[0];
+        $truefalse = 0;
+        for ($i = 1; $i < count($nums); $i++) {
+            $mustarraynum = $oldnumber + 1;
+            if ($mustarraynum != $nums[$i]) {
+                $truefalse++;
+            }
+            $oldnumber = $nums[$i];
+        }
+        return $truefalse;
+    }
+}
+
+$arraynums = [1, 10, 100, 1000];
+$Solution = new Solution();
+echo $Solution->minOperations($arraynums);
+?>
