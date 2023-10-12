7e0ba65986a311dae60c7f4b0731a4479da659bf
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2023-10-13 02:10:21 +0300

    10th Question Wrong Answer
    Wrong Answer 305 / 306 testcases passed

---
+++ LeetCode/Problem 10 - Pow(x n)/solution1.php
@@ -0,0 +1,17 @@
+<?php
+
+class Solution
+{
+
+    /**
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow($x, $n)
+    {
+        return pow($x, $n);
+    }
+}
+
+?>
